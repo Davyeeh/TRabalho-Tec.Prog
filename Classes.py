cc5cfdcf64bffc45d8373a30978d92ed50c0f839
Gabriel Cavalcante <gmoreirac26@gmail.com>
2025-02-12 17:13:44 -0300

    classes
    adicionei a classe tweet e implementei algumas coisas na classe perfil

--- Classes.py
+++ Classes.py
@@ -40,6 +40,34 @@ class Perfil():
     def is_ativo(self):
         return self.__ativo
 
+#Métodos adicionados pelo gabriel
+
+     # Método para adicionar um seguidor ao perfil
+    def add_seguidor(self, perfil):
+        if perfil not in self.__seguidores:
+            self.__seguidores.append(perfil)
+    
+     # Método para seguir outro perfil
+    def add_seguidos(self, perfil):
+        if perfil not in self.__seguidos:
+            self.__seguidos.append(perfil)
+
+    # Método para adicionar um tweet à lista do perfil
+    def add_tweet(self, tweet):
+        self.__tweets.append(tweet)
+        self.__tweets.sort(key=lambda t: t.get_data_postagem())  # Ordena os tweets pela data
+    
+    # Método para obter todos os tweets do perfil, ordenados por data
+    def get_tweets(self):
+        return sorted(self.__tweets, key=lambda t: t.get_data_postagem())
+    
+    # Método para obter um tweet específico pelo ID
+    def get_tweet(self, tweet_id):
+        for tweet in self.__tweets:
+            if tweet.get_id() == tweet_id:
+                return tweet
+        return None  # Retorna None se o tweet não for encontrado
+
 # Classe PessoaFisica que represenra um perfil de pessoa física
 class PessoaFisica(Perfil):
     def __init__(self, usuario, cpf):
@@ -58,7 +86,7 @@ class PessoaJuridica(Perfil):
     
     # Função para obter o CNPJ do dono do perfil
     def get_cnpj(self): 
-        return self.cnpj
+        return self.__cnpj
 
 # Exceção para usuário já cadastrado
 class UJCException(Exception):
@@ -87,3 +115,27 @@ class RepositorioUsuarios():
                 return usuario
         # se não, retorna None
         return None
+    
+
+# Gabriel
+
+class Tweet:
+    def __init__(self, usuario, mensagem, gerador_id):
+        self.__id = next(gerador_id) # Gera o ID único usando a função geradora
+        self.__usuario = usuario # Nome do usuário que postou
+        self.__mensagem = mensagem # Conteúdo do tweet
+        self.__data_postagem = datetime.now() # Data e hora da postagem geradas automaticamente
+        
+    # Métodos de acesso (getters)
+
+    def get_id(self):
+        return self.__id
+
+    def get_usuario(self):
+        return self.__usuario
+
+    def get_mensagem(self):
+        return self.__mensagem
+
+    def get_data_postagem(self):
+        return self.__data_postagem
