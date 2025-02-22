e7529153c14c2e13a827958ebafe06ccfa8ea81c
Davi Araújo <111143487+Davyeeh@users.noreply.github.com>
2025-02-22 18:28:51 +0000

    Remover definições de exceções específicas do sistema e importar de um módulo separado

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
 from datetime import datetime
-
-# Exceções específicas do sistema
-
-class PEException(Exception):  # Perfil Existente
-    pass
-
-class PIException(Exception):  # Perfil Inexistente
-    pass
-
-class PDException(Exception):  # Perfil Desativado
-    pass
-
-class MFPException(Exception):  # Mensagem Fora do Padrão
-    pass
-
-class SIException(Exception):  # Seguidor Inválido
-    pass
-
-# Exceção para usuário já cadastrado
-class UJCException(Exception):
-    pass
-
-class UNCException(Exception):  # Usuário Não Cadastrado
-    pass
-
+from exception import PEException, PIException, PDException, MFPException, SIException, UJCException, UNCException
 
 # Início das classes
 
